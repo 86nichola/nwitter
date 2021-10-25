f8996550874636ba437a35550ae73bcf570bff22
nicholaKim <86nichola@gmail.com>
2021-10-26 00:37:35 +0900

    fix error webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
         },
       },
       {
-        test: /\.css/i,
+        test: /\.css$/i,
         exclude: /node_modules/,
         use: ["style-loader", "css-loader"],
       },
